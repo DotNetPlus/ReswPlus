f4bb418fb1b1a114a5954a14829b78cdc93931a8
Rudy Huyn <rudyhuyn@gmail.com>
2019-10-13 23:06:46 -0700

    Fix Html parser unit tests

--- src/Library/UnitTests/ReswPlusLibUnitTestProject/HtmlParserUnitTest.cs
+++ src/Library/UnitTests/ReswPlusLibUnitTestProject/HtmlParserUnitTest.cs
@@ -24,23 +24,23 @@ namespace ReswPlusLibUnitTests
         {
             var res = HTMLParser.Parse("Hello").ToList();
             Assert.AreEqual(res.Count(), 1);
-            Assert.IsTrue(CheckRun(res[0], "Hello", FontWeights.Normal, FontStyle.Normal, TextDecorations.None));
+            Assert.IsTrue(CheckRun((Run)res[0], "Hello", FontWeights.Normal, FontStyle.Normal, TextDecorations.None));
 
             res = HTMLParser.Parse("  Hello").ToList();
             Assert.AreEqual(res.Count(), 1);
-            Assert.IsTrue(CheckRun(res[0], "  Hello", FontWeights.Normal, FontStyle.Normal, TextDecorations.None));
+            Assert.IsTrue(CheckRun((Run)res[0], "  Hello", FontWeights.Normal, FontStyle.Normal, TextDecorations.None));
 
             res = HTMLParser.Parse("Hello  ").ToList();
             Assert.AreEqual(res.Count(), 1);
-            Assert.IsTrue(CheckRun(res[0], "Hello  ", FontWeights.Normal, FontStyle.Normal, TextDecorations.None));
+            Assert.IsTrue(CheckRun((Run)res[0], "Hello  ", FontWeights.Normal, FontStyle.Normal, TextDecorations.None));
 
             res = HTMLParser.Parse("  Hello World ").ToList();
             Assert.AreEqual(res.Count(), 1);
-            Assert.IsTrue(CheckRun(res[0], "  Hello World ", FontWeights.Normal, FontStyle.Normal, TextDecorations.None));
+            Assert.IsTrue(CheckRun((Run)res[0], "  Hello World ", FontWeights.Normal, FontStyle.Normal, TextDecorations.None));
 
             res = HTMLParser.Parse(" ").ToList();
             Assert.AreEqual(res.Count(), 1);
-            Assert.IsTrue(CheckRun(res[0], " ", FontWeights.Normal, FontStyle.Normal, TextDecorations.None));
+            Assert.IsTrue(CheckRun((Run)res[0], " ", FontWeights.Normal, FontStyle.Normal, TextDecorations.None));
 
             res = HTMLParser.Parse("").ToList();
             Assert.AreEqual(res.Count(), 0);
@@ -51,19 +51,19 @@ namespace ReswPlusLibUnitTests
         {
             var res = HTMLParser.Parse("<b>Hello</b>").ToList();
             Assert.AreEqual(res.Count(), 1);
-            Assert.IsTrue(CheckRun(res[0], "Hello", FontWeights.Bold, FontStyle.Normal, TextDecorations.None));
+            Assert.IsTrue(CheckRun((Run)res[0], "Hello", FontWeights.Bold, FontStyle.Normal, TextDecorations.None));
 
             res = HTMLParser.Parse("<i>  Hello</i>").ToList();
             Assert.AreEqual(res.Count(), 1);
-            Assert.IsTrue(CheckRun(res[0], "  Hello", FontWeights.Normal, FontStyle.Italic, TextDecorations.None));
+            Assert.IsTrue(CheckRun((Run)res[0], "  Hello", FontWeights.Normal, FontStyle.Italic, TextDecorations.None));
 
             res = HTMLParser.Parse("<u>Hello  </u>").ToList();
             Assert.AreEqual(res.Count(), 1);
-            Assert.IsTrue(CheckRun(res[0], "Hello  ", FontWeights.Normal, FontStyle.Normal, TextDecorations.Underline));
+            Assert.IsTrue(CheckRun((Run)res[0], "Hello  ", FontWeights.Normal, FontStyle.Normal, TextDecorations.Underline));
 
             res = HTMLParser.Parse("<s>  Hello World </s>").ToList();
             Assert.AreEqual(res.Count(), 1);
-            Assert.IsTrue(CheckRun(res[0], "  Hello World ", FontWeights.Normal, FontStyle.Normal, TextDecorations.Strikethrough));
+            Assert.IsTrue(CheckRun((Run)res[0], "  Hello World ", FontWeights.Normal, FontStyle.Normal, TextDecorations.Strikethrough));
         }
 
         [UITestMethod]
@@ -71,20 +71,20 @@ namespace ReswPlusLibUnitTests
         {
             var res = HTMLParser.Parse("<b>Hello</b> World").ToList();
             Assert.AreEqual(res.Count(), 2);
-            Assert.IsTrue(CheckRun(res[0], "Hello", FontWeights.Bold, FontStyle.Normal, TextDecorations.None));
-            Assert.IsTrue(CheckRun(res[1], " World", FontWeights.Normal, FontStyle.Normal, TextDecorations.None));
+            Assert.IsTrue(CheckRun((Run)res[0], "Hello", FontWeights.Bold, FontStyle.Normal, TextDecorations.None));
+            Assert.IsTrue(CheckRun((Run)res[1], " World", FontWeights.Normal, FontStyle.Normal, TextDecorations.None));
 
             res = HTMLParser.Parse("<b>Hello</b> World <i>!!!</i>").ToList();
             Assert.AreEqual(res.Count(), 3);
-            Assert.IsTrue(CheckRun(res[0], "Hello", FontWeights.Bold, FontStyle.Normal, TextDecorations.None));
-            Assert.IsTrue(CheckRun(res[1], " World ", FontWeights.Normal, FontStyle.Normal, TextDecorations.None));
-            Assert.IsTrue(CheckRun(res[2], "!!!", FontWeights.Normal, FontStyle.Italic, TextDecorations.None));
+            Assert.IsTrue(CheckRun((Run)res[0], "Hello", FontWeights.Bold, FontStyle.Normal, TextDecorations.None));
+            Assert.IsTrue(CheckRun((Run)res[1], " World ", FontWeights.Normal, FontStyle.Normal, TextDecorations.None));
+            Assert.IsTrue(CheckRun((Run)res[2], "!!!", FontWeights.Normal, FontStyle.Italic, TextDecorations.None));
 
             res = HTMLParser.Parse("Welcome <b>Hello</b> World").ToList();
             Assert.AreEqual(res.Count(), 3);
-            Assert.IsTrue(CheckRun(res[0], "Welcome ", FontWeights.Normal, FontStyle.Normal, TextDecorations.None));
-            Assert.IsTrue(CheckRun(res[1], "Hello", FontWeights.Bold, FontStyle.Normal, TextDecorations.None));
-            Assert.IsTrue(CheckRun(res[2], " World", FontWeights.Normal, FontStyle.Normal, TextDecorations.None));
+            Assert.IsTrue(CheckRun((Run)res[0], "Welcome ", FontWeights.Normal, FontStyle.Normal, TextDecorations.None));
+            Assert.IsTrue(CheckRun((Run)res[1], "Hello", FontWeights.Bold, FontStyle.Normal, TextDecorations.None));
+            Assert.IsTrue(CheckRun((Run)res[2], " World", FontWeights.Normal, FontStyle.Normal, TextDecorations.None));
         }
 
         [UITestMethod]
@@ -92,15 +92,15 @@ namespace ReswPlusLibUnitTests
         {
             var res = HTMLParser.Parse("<b><i>Hello</i></b> World").ToList();
             Assert.AreEqual(res.Count(), 2);
-            Assert.IsTrue(CheckRun(res[0], "Hello", FontWeights.Bold, FontStyle.Italic, TextDecorations.None));
-            Assert.IsTrue(CheckRun(res[1], " World", FontWeights.Normal, FontStyle.Normal, TextDecorations.None));
+            Assert.IsTrue(CheckRun((Run)res[0], "Hello", FontWeights.Bold, FontStyle.Italic, TextDecorations.None));
+            Assert.IsTrue(CheckRun((Run)res[1], " World", FontWeights.Normal, FontStyle.Normal, TextDecorations.None));
 
             res = HTMLParser.Parse("<b><i>Hell</i>o</b> <i>World</i>").ToList();
             Assert.AreEqual(res.Count(), 4);
-            Assert.IsTrue(CheckRun(res[0], "Hell", FontWeights.Bold, FontStyle.Italic, TextDecorations.None));
-            Assert.IsTrue(CheckRun(res[1], "o", FontWeights.Bold, FontStyle.Normal, TextDecorations.None));
-            Assert.IsTrue(CheckRun(res[2], " ", FontWeights.Normal, FontStyle.Normal, TextDecorations.None));
-            Assert.IsTrue(CheckRun(res[3], "World", FontWeights.Normal, FontStyle.Italic, TextDecorations.None));
+            Assert.IsTrue(CheckRun((Run)res[0], "Hell", FontWeights.Bold, FontStyle.Italic, TextDecorations.None));
+            Assert.IsTrue(CheckRun((Run)res[1], "o", FontWeights.Bold, FontStyle.Normal, TextDecorations.None));
+            Assert.IsTrue(CheckRun((Run)res[2], " ", FontWeights.Normal, FontStyle.Normal, TextDecorations.None));
+            Assert.IsTrue(CheckRun((Run)res[3], "World", FontWeights.Normal, FontStyle.Italic, TextDecorations.None));
 
         }
     }
